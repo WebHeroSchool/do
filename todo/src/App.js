4d180a0751b28cf8a17d3267cc9976c9e895866a
ElenaSindi <sindielena@icloud.com>
2021-02-15 22:43:01 +0300

    добавила примеры в app.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+const item = 43;
+const elem = false;
+const element = true;
+const a = 5;
+const b = 4;
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p style={{ color: 'red'}}>Hello World!</p>
+        <p>{item}</p>
+        <p>{elem && 'elem is true'}</p>
+        <p>{element ? 'element is true' : 'element is false'}</p>
+        <p>{a+b}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
