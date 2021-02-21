eac64d27aff118204b4166c5fce1644646f0ad27
ElenaSindi <sindielena@icloud.com>
2021-02-21 18:02:40 +0300

    создала список дел в компоненте App

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -3,13 +3,18 @@ import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItem/InputItem';
 import Footer from '../Footer/Footer';
 
-const todoItem = 'Написать приложение';
-
-const App = () => (<div>
-      <h1>Важные дела:</h1>
-      <InputItem />
-      <ItemList todoItem = { todoItem } />
-      <Footer count = {3} />
-</div>);
+const App = () => { 
+      const items = [
+            { value: 'Написать новое приложение'},
+            { value: 'Прописать props'},
+            { value: 'Сделать все дела'}
+      ];
+      return (<div>
+            <h1>Важные дела:</h1>
+            <InputItem />
+            <ItemList items = { items } />
+            <Footer count = {3} />
+      </div>);
+};
 
 export default App;
