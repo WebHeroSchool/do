81a1fc6cfff99ac9b18d2cf0a6b9f7262e60702f
ElenaSindi <sindielena@icloud.com>
2021-03-01 19:13:36 +0300

    стилизуем приложение

--- todo/src/components/Footer/Footer.js
+++ todo/src/components/Footer/Footer.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
-const Footer = ({ count }) => (<div>Вот что осталось: { count }</div>);
+const Footer = ({ count }) => (<div>
+      <div>Вот что осталось: { count }</div>
+      <ButtonGroup size="small" aria-label="small outlined button group">
+        <Button>Все</Button>
+        <Button>что делать</Button>
+        <Button>что сделано</Button>
+      </ButtonGroup>
+      <Button variant="outlined" size="medium" color="primary">
+          Удалить выполненные дела
+      </Button>
+   
+     
+   </div>);
 
 export default Footer;
--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import Item from '../Item/Item';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const ItemList = ({ items }) => (<ul>
-      { items.map(item => ( <li key={item.value}><Item value={ item.value } isDone={item.isDone} /></li> ))}
+      { items.map(item => ( <li key={item.value}>
+            <Checkbox />
+            <Item value={ item.value } isDone={item.isDone} /> 
+            <Button variant="outlined" size="medium" color="primary">
+                  Удалить
+            </Button>
+            <IconButton aria-label="delete">
+                  <DeleteIcon />
+            </IconButton>
+            </li> ))}
 </ul>);
 
 export default ItemList;
