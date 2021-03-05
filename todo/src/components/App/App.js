168a39f6dda119418dbf695dc9d7d65dfe4ba9e1
ElenaSindi <sindielena@icloud.com>
2021-03-05 13:41:33 +0300

    state

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -5,23 +5,26 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-  items=[
-    { value: 'Написать новое приложение',
-      isDone: true
-    },
-    { value: 'Прописать props',
-      isDone: false
-    },
-    { value: 'Сделать все дела',
-      isDone: true
-    }
-  ];
+  state = {
+    items: [
+      { value: 'Написать новое приложение',
+        isDone: true
+      },
+      { value: 'Прописать props',
+        isDone: false
+      },
+      { value: 'Сделать все дела',
+        isDone: true
+      }
+    ]
+  };
+  
   render() {
     return ( 
           <div className={styles.block}>
             <h1 className={styles.title}>Важные дела:</h1>
             <InputItem />
-            <ItemList items = { this.items } />
+            <ItemList items = { this.state.items } />
             <Footer count = {3} />
           </div>);
   }
